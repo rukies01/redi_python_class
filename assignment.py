0bb104a44893ddd640bd3985fcf1a089d1653370
Rukayyat Adelekan <132057787+rukies01@users.noreply.github.com>
2025-10-01 14:51:53 +0200

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,55 @@
+# Write a program that will do the following:
+# 1. Continuously loop until the user enters the empty string. They can do
+#    this by simply pressing Enter or Return without entering in any
+#    characters.
+# 2. Within the loop, ask the user to enter a string. Again, if they just 
+#    press the Enter or Return key, this results in an "empty string". 
+#    If they entered the empty string, end the loop. The empty string
+#    in code is "". Otherwise...
+# 3. Print out four different versions of the string using various string 
+#    functions:
+#       a) Print out the string with only the first letter capitalized. 
+#       b) Print out the string in all lowercase.
+#       c) Print out the string in title case. 
+#       d) Print out the string in all uppercase.
+# 4. Then loop around and ask the user for another string, following the 
+#    logic above.
+#
+# Here is how the output might look like, but feel free to experiment with
+# your code:
+#
+#   Enter a string: <user presses Enter>
+#   Thanks for playing! <and the program ends>
+#
+# <Run the program again>
+#
+#   Enter a string: This Is a STRING with MiXeD casing.
+#   First letter capitalized: This is a string with mixed casing.
+#   All lowercase:            this is a string with mixed casing.
+#   Title case:               This Is A String With Mixed Casing.
+#   All uppercase:            THIS IS A STRING WITH MIXED CASING.
+#   Enter a string: this is ANOTHER string with mixed casing.
+#   First letter capitalized: This is another string with mixed casing.
+#   All lowercase:            this is another string with mixed casing.
+#   Title case:               This Is Another String With Mixed Casing.
+#   All uppercase:            THIS IS ANOTHER STRING WITH MIXED CASING. 
+#   Enter a string: <user presses Enter>
+#   Thanks for playing! <and the program ends>
+
+
+#CLASS 3
+#Practice
+#● List: Write a program that will use a while loop to obtain a set of numbers from the user.
+#When the user enters an 'x' or an 'X', sum the numbers and display both the sum and
+#the average of their numbers.
+#○ Answer is in ListsSumsAndAverages
+#● Dictionary: Write a program that will ask the user for a number and then calculate all the
+#squares from 1 to their number and store them in a dictionary. Print out the dictionary at
+#the end to see the results.
+#○ Answer is in NumberSquarer
+#● String: Write a Python program that inserts "ReDI" into the string "Welcome to the
+#School of Digital Integration" so that it reads "Welcome to the ReDI School of
+#Digital Integration".
+
+
+#CLASS 4
